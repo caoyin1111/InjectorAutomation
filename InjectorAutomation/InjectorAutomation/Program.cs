76abfce54a3e441ff687a1fb9df303c27654251c
caoyin1111 <55869022+caoyin1111@users.noreply.github.com>
2020-07-17 09:34:06 +0800

    第一次创建

---
+++ InjectorAutomation/InjectorAutomation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InjectorAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
